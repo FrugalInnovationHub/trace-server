3f32ced64e1dc13e83bac19871974fd49b5febfa
Ankit09osr <ankit09osr@gmail.com>
2018-11-12 18:25:48 -0800

    Add Product post method

--- components/product.js
+++ components/product.js
@@ -1,28 +1,71 @@
-// Product handler
+/*
+ * File for Product handler
+ */
+
+// Dependencies
+const dbConfig = require('../db/dbConfig');
+const dbUtils = require('../db/dbUtils');
 
 // Container for Product methods
 const product = {};
 
 product.get = function(req, res) {
-  res.type('application/json');
-  res.status(405).send({});
+  res.type('application/json').status(405).send({});
 };
 
+// Product POST request
 product.post = function(req, res) {
-  console.log('Request : ', req.body);
+  let { productName, productNumber, category, manufacturer } = req.body;
 
-  res.type('application/json');
-  res.status(405).send({'Error' : 'Missing required fields'});
+  // Check if all required fields are present
+  productName = typeof productName === 'undefined' ? false : productName;
+  productNumber = typeof productNumber === 'undefined' ? false : productNumber;
+  category = typeof category === 'undefined' ? false : category;
+  manufacturers = Array.isArray(manufacturer) ? manufacturer : false;
+
+  if (productName && productNumber && category && manufacturers) {
+    // Insert values in product_add_table
+    let query = "Insert into product_add_table (product_name, product_id, category) values (?, ?, ?)";
+    let values = [productName, productNumber, category];
+    dbUtils.query(query, values)
+    .then(function(results){
+      // Insert values in manufacturer_table
+      query = "Insert into manufacturer_table (manufacturer_id, manufacturer_name) values (?, ?)";
+      var manufacturersPromise = manufacturers.map((manufacturer) => {
+        let { manufacturerName, manufacturerId } = manufacturer;
+        values = [manufacturerId, manufacturerName];
+        return dbUtils.query(query, values);
+      });
+      return Promise.all(manufacturersPromise);
+    })
+    .then(function(results) {
+      // Insert values in product_details_table
+      query = "Insert into product_details_table (product_id, manufacturer_id) values (?,?)";
+      var manufacturersPromise = manufacturers.map((manufacturer) => {
+        let { manufacturerId } = manufacturer;
+        values = [productNumber, manufacturerId];
+        return dbUtils.query(query, values);
+      });
+
+      return Promise.all(manufacturersPromise);
+    })
+    .then(function(results) {
+      res.type('application/json').status(200).send({});
+    })
+    .catch(function(err) {
+      res.type('application/json').status(500).send({'Error' : 'Internal Server Error'});
+    });
+  } else {
+    res.type('application/json').status(405).send({'Error' : 'Missing required fields'});
+  }
 };
 
 product.put = function(req, res) {
-  res.type('application/json');
-  res.status(405).send({});
+  res.type('application/json').status(405).send({});
 };
 
 product.delete = function(req, res) {
-  res.type('application/json');
-  res.status(405).send({});
+  res.type('application/json').status(405).send({});
 };
 
 // Export the module
---
+++ db/dbUtils.js
@@ -0,0 +1,24 @@
+/*
+ * Utility functions for sending data
+ */
+const mysql = require('mysql');
+const dbConfig = require('./dbConfig');
+
+const dbUtils = {};
+
+dbUtils.query = function (currentQuery, args) {
+  return new Promise(function(resolve, reject) {
+    const connection = mysql.createConnection(dbConfig);
+    connection.connect();
+    connection.query(currentQuery, args, function(err, results) {
+      if(err) {
+        reject(err);
+      }
+      resolve(results);
+    });
+    connection.end();
+  });
+};
+
+// Export the module
+module.exports = dbUtils;
